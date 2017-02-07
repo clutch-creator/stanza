96167e00c45e3647026b0f82337321b96dd6b958
Bruno Mota <bruno12mota@gmail.com>
2017-02-07 11:04:34 +0000

    eslint lint and lint-fix commands

---
+++ src/scripts/lint/index.js
@@ -0,0 +1,23 @@
+import { CLIEngine } from 'eslint';
+
+export default (fix) => {
+  const cli = new CLIEngine({
+    baseConfig: {
+      extends: ['stanza'],
+    },
+    useEslintrc: false,
+    extensions: ['.js', '.jsx'],
+    fix,
+  });
+
+  const report = cli.executeOnFiles(['src/']);
+  const formatter = cli.getFormatter('codeframe');
+
+  console.log(formatter(report.results));
+
+  if (fix) {
+    CLIEngine.outputFixes(report);
+  } else if (report.errorCount) {
+    process.exit(1); // exit with error
+  }
+};
