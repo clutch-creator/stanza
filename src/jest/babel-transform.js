046486b2e7c0b25153a1cb6d399d98cbe5fb8a35
Bruno Mota <bruno12mota@gmail.com>
2017-02-14 11:33:21 +0100

    jest enfore commonjs modules transpilation

--- src/jest/babel-transform.js
+++ src/jest/babel-transform.js
@@ -3,9 +3,12 @@ import babelJest from 'babel-jest';
 module.exports = babelJest.createTransformer({
   presets: [
     'react',
-    'es2015',
+    ['es2015', { modules: false }],
     'stage-0',
   ],
-  plugins: ['transform-runtime'],
+  plugins: [
+    'transform-runtime',
+    'transform-es2015-modules-commonjs',
+  ],
   babelrc: false,
 });
