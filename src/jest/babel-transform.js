d567b1d3cefa6fd7db629cc836283f5cbe9a2485
Bruno Mota <bruno12mota@gmail.com>
2018-10-06 16:49:03 +0100

    jest babel transform fix

--- src/jest/babel-transform.js
+++ src/jest/babel-transform.js
@@ -1,11 +1,22 @@
 import babelJest from 'babel-jest';
 
 module.exports = babelJest.createTransformer({
-  presets: ['react', ['env', { modules: false }], 'stage-0'],
+  presets: ['@babel/react', ['@babel/env', { modules: false }]],
   plugins: [
-    'transform-decorators-legacy',
-    'transform-runtime',
-    'transform-es2015-modules-commonjs',
+    '@babel/plugin-proposal-class-properties',
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        decoratorsBeforeExport: true,
+      },
+    ],
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-import-meta',
+    '@babel/plugin-transform-modules-commonjs',
+    '@babel/plugin-transform-react-jsx-self',
+    '@babel/plugin-transform-react-jsx-source',
+    '@babel/plugin-transform-runtime',
   ],
   babelrc: false,
 });
