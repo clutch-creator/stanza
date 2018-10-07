adcb6a97539944c612a1418d7d511497a058183e
Bruno Mota <bruno12mota@gmail.com>
2018-10-07 22:15:46 +0100

    fix jest babel config

--- src/jest/babel-transform.js
+++ src/jest/babel-transform.js
@@ -3,16 +3,17 @@ import babelJest from 'babel-jest';
 module.exports = babelJest.createTransformer({
   presets: ['@babel/react', ['@babel/env', { modules: false }]],
   plugins: [
-    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-export-extensions',
+    '@babel/plugin-proposal-export-default-from',
     [
       '@babel/plugin-proposal-decorators',
       {
-        decoratorsBeforeExport: true,
+        legacy: true,
       },
     ],
-    '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-syntax-import-meta',
+    ['@babel/plugin-proposal-class-properties', { loose: false }],
     '@babel/plugin-transform-modules-commonjs',
     '@babel/plugin-transform-react-jsx-self',
     '@babel/plugin-transform-react-jsx-source',
